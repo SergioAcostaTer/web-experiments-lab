209483663190f35354340de9910c14b7642a3da1
Sergio Acosta <sergioacostaquintana@gmail.com>
2023-08-07 21:25:59 +0100

    live users

--- Room.js
+++ Room.js
@@ -43,6 +43,19 @@ class Room {
     roomNamespace.on("connection", (socket) => {
       console.log(`User connected to ${this.roomName}`);
 
+      socket.on("joinRoom", (username) => {
+        console.log(`${username} joined ${this.roomName}`);
+        this.users++;
+
+        socket.emit("userCount", this.users);
+      });
+
+      socket.on("leaveRoom", (username) => {
+        console.log(`${username} left ${this.roomName}`);
+        this.users--;
+        socket.emit("userCount", this.users);
+      });
+
       socket.emit("songDetails", this.musicQueue.song);
 
       socket.on("newMessage", (message) => {
