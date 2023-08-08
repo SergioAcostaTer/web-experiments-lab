d893955d99b6ee924386479b2eea4da027c15e53
Sergio Acosta <sergioacostaquintana@gmail.com>
2023-08-08 03:21:52 +0100

    back to blob just to test vercel

--- services/getAudio.js
+++ services/getAudio.js
@@ -30,23 +30,23 @@ async function getAudio(name, artists, cover, spotifyDuration) {
 
   const audio = searchResults.items[indexOfClosest];
 
-  // const stream = await ytdl(audio.url, {
-  //   format: "mp3",
-  //   quality: "highestaudio",
-  //   });
-
-  //   const blob = await streamToBlob(stream);
-  // const base64 = await blob.arrayBuffer().then((buffer) => {
-  //   return Buffer.from(buffer).toString("base64");
-  // });
-
-  const audioInfo = await ytdl.getInfo(audio.url);
-
-  const audioFormat = ytdl.chooseFormat(audioInfo.formats, {
+  const stream = await ytdl(audio.url, {
+    format: "mp3",
     quality: "highestaudio",
+    });
+
+    const blob = await streamToBlob(stream);
+  const base64 = await blob.arrayBuffer().then((buffer) => {
+    return Buffer.from(buffer).toString("base64");
   });
 
-  const audioUrl = audioFormat.url;
+  // const audioInfo = await ytdl.getInfo(audio.url);
+
+  // const audioFormat = ytdl.chooseFormat(audioInfo.formats, {
+  //   quality: "highestaudio",
+  // });
+
+  // const audioUrl = audioFormat.url;
 
 
   const colors = await getColorFromURL(cover);
@@ -61,8 +61,8 @@ async function getAudio(name, artists, cover, spotifyDuration) {
   const audioDetails = {
     name: name,
     artists: artists.map((artist) => artist.name),
-    // url: `data:audio/mpeg;base64,${base64}`,
-    url: audioUrl,
+    url: `data:audio/mpeg;base64,${base64}`,
+    // url: audioUrl,
     duration: audio.duration,
     cover: cover,
     currentTime: 0,
